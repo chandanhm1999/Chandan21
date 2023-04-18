401575f7846af4cfd5171f2a72c08346ac9e1b2f
CHANDAN HM <109410990+chandanhm1999@users.noreply.github.com>
2023-04-18 22:37:49 +0530

    Update Testimonial.js

--- src/components/testimonial/Testimonial.js
+++ src/components/testimonial/Testimonial.js
@@ -208,7 +208,7 @@ const Testimonial = () => {
                 />
                 <div className="w-full flex flex-col justify-end">
                   <p className="text-xs uppercase text-yellow-300 tracking-wide mb-2">
-                    Edyodha
+                    Edyoda
                   </p>
                   <h3 className="text-2xl font-bold">Vaibhav</h3>
                   <p className="text-base tracking-wide text-gray-500">
